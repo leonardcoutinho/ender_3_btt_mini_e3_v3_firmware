985a04a6e2f84f891c00ac60965155d234c9dcfc
Leonardo Coutinho <leonardcoutinho@gmail.com>
2026-03-21 01:42:30 -0300

    Update default.js

--- builds/default.js
+++ builds/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-    board_env: "STM32G0B1RE",
+    board_env: "STM32G0B1RE_btt",
     min_version: "2.1.2",
     meta: {
         stable_name: "ender_3_skr_e3v3-{{marlin_version}}-default-{{uid}}",
