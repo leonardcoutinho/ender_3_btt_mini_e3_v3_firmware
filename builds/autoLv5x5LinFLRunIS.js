ced4fe4200fa732f023f91838d3323a47b82782f
Leonardo Coutinho <leonardcoutinho@gmail.com>
2026-03-21 01:20:46 -0300

    Update autoLv5x5LinFLRunIS.js

--- builds/autoLv5x5LinFLRunIS.js
+++ builds/autoLv5x5LinFLRunIS.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: "builds/autoLevel5x5LinFLRun.js",
     include: "builds/features/in_shap.js",
     meta: {
-        stable_name: "ender_3_4.2.2-{{marlin_version}}-autoLv5x5LinFLRunIS-{{uid}}",
-        nightly_name: "ender_3_4.2.2-{{current_date}}-autoLv5x5LinFLRunIS-{{uid}}"
+        stable_name: "ender_3_skr_e3v3-{{marlin_version}}-autoLv5x5LinFLRunIS-{{uid}}",
+        nightly_name: "ender_3_skr_e3v3-{{current_date}}-autoLv5x5LinFLRunIS-{{uid}}"
     }
 };
