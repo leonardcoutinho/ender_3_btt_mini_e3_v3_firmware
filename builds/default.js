9282fc5a70e4a0bfd4f1bb7ec10f60da9d9eebb7
Leonardo Coutinho <leonardcoutinho@gmail.com>
2026-03-22 00:17:29 -0300

    Update default.js

--- builds/default.js
+++ builds/default.js
@@ -6,8 +6,8 @@ module.exports = {
         nightly_name: "ender_3_skr_e3v3-{{current_date}}-default-{{uid}}"
     },
     based_on: {
-        repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: ["/config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 3.0/"],
+        repo: "https://github.com/leonardcoutinho/ender_3_btt_mini_e3_v3_firmware/",
+        path: "/config/examples/Creality/Ender-3/BigTreeTech_SKR_Mini_E3_3.0/",
         stable_branch: "release-{{marlin_version}}",
         nightly_branch: "bugfix-2.1.x"
     }
