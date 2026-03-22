df37a49f6c35197075fd76a77bb95632141da2f2
Leonardo Coutinho <leonardcoutinho@gmail.com>
2026-03-21 21:17:48 -0300

    Delete builds/baseLin.js

--- builds/baseLin.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: "builds/base.js",
-    include: "builds/features/linearAdvance.js",
-    meta: {
-        stable_name: "ender_3_skr_e3v3-{{marlin_version}}-base_lin-{{uid}}",
-        nightly_name: "ender_3_skr_e3v3-{{current_date}}-base_lin-{{uid}}"
-    }
-};
