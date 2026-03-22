115b87ded38ecf57e5eadca1049d982c8c2111ab
Leonardo Coutinho <leonardcoutinho@gmail.com>
2026-03-21 21:18:04 -0300

    Delete builds/autoLv5x5LinFLRunIS.js

--- builds/autoLv5x5LinFLRunIS.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: "builds/autoLevel5x5LinFLRun.js",
-    include: "builds/features/in_shap.js",
-    meta: {
-        stable_name: "ender_3_skr_e3v3-{{marlin_version}}-autoLv5x5LinFLRunIS-{{uid}}",
-        nightly_name: "ender_3_skr_e3v3-{{current_date}}-autoLv5x5LinFLRunIS-{{uid}}"
-    }
-};
