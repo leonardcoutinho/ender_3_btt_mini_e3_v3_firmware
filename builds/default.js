f421be6b035d7270c9e79e609c1e646152d6d75f
Leonardo Coutinho <leonardcoutinho@gmail.com>
2026-03-22 02:45:22 -0300

    Update board environment and configuration paths

--- builds/default.js
+++ builds/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-    board_env: "STM32F103RE_creality",
+    board_env: "STM32G0B1RE_btt",
     min_version: "2.1.2",
     meta: {
         stable_name: "ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
@@ -7,7 +7,7 @@ module.exports = {
     },
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: "/config/examples/Creality/Ender-3/MEEB-3DP/",
+        path: "/config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 3.0/",
         stable_branch: "release-{{marlin_version}}",
         nightly_branch: "bugfix-2.1.x"
     }
